f88ca137c9ca2ace608a10a9d0090f46c5791150
Naman1000 <91007344+Naman1000@users.noreply.github.com>
2025-04-15 19:20:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your code here
+let inputbox = document.getElementById("evaluatedText");
+let letterCount = document.getElementById("letterCount");
+
+inputbox.addEventListener('input',()=>{
+
+	letterCount.textContent = inputbox.value.length;
+})
